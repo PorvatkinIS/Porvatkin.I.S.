8cd8d55e6577c4fb9807b52fdce83c3ad9842e37
PorvatkinIS <144272507+PorvatkinIS@users.noreply.github.com>
2023-10-31 12:09:15 +0300

    Update Задание 8.py

--- Занятие 5/Задание 8.py
+++ Занятие 5/Задание 8.py
@@ -1,4 +1,15 @@
-def f(s):
-    count = s.count("a")
-    s = s.replace("a", "")
-    return count
+def f() -> int:
+    currentLength = maxLength = predNumber = 0
+    currentNumber = int(input())
+    while currentNumber != 0:
+        if currentNumber == predNumber:
+            currentLength += 1
+        else:
+            predNumber = currentNumber
+            maxLength = max(currentLength, maxLength)
+            currentLength = 1
+        currentNumber = int(input())
+    return max(currentLength, maxLength)
+
+maxLength = f()
+print(maxLength)
